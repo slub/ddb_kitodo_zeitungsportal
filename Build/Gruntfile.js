8c70570eecd0d6f5943cd467c557dde20564ba08
Florian Schölzel <fschoelzel.kontakt@gmail.com>
2024-06-25 17:05:59 +0200

    Rename Javascript to JavaScript - update Readme - fresh packages build

--- Build/Gruntfile.js
+++ Build/Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
                 },
                 files: {
                     "Resources/Public/Css/ddbKitodoZeitungsportal.css" : [
-                        "../dlf/Resources/Public/Javascript/OpenLayers/openlayers.css",
+                        "../dlf/Resources/Public/JavaScript/OpenLayers/openlayers.css",
                         "Resources/Private/Less/all.less",
                     ],
                 }
@@ -26,30 +26,30 @@ module.exports = function(grunt) {
                 },
                 files: {
                     "Resources/Public/JavaScript/ddbKitodoZeitungsportal.js" : [
-                        '../dlf/Resources/Public/Javascript/jQueryUI/jquery-ui-mouse-slider-resizable-autocomplete.js',
-                        '../dlf/Resources/Public/Javascript/OpenLayers/openlayers.js',
+                        '../dlf/Resources/Public/JavaScript/jQueryUI/jquery-ui-mouse-slider-resizable-autocomplete.js',
+                        '../dlf/Resources/Public/JavaScript/OpenLayers/openlayers.js',
                         // Viewer
                         // highlight exact word in search results
-                        '../dlf/Resources/Public/Javascript/PageView/Utility.js',
-                        '../dlf/Resources/Public/Javascript/PageView/OL.js',
+                        '../dlf/Resources/Public/JavaScript/PageView/Utility.js',
+                        '../dlf/Resources/Public/JavaScript/PageView/OL.js',
                         // Override text highlighting color
-                        '../dlf/Resources/Public/Javascript/PageView/OLStyles.js',
+                        '../dlf/Resources/Public/JavaScript/PageView/OLStyles.js',
                         'Resources/Private/JavaScript/Kitodo/PageView/OLStyles.js',
-                        '../dlf/Resources/Public/Javascript/PageView/OLSources.js',
-                        '../dlf/Resources/Public/Javascript/PageView/AltoParser.js',
-                        '../dlf/Resources/Public/Javascript/PageView/AnnotationParser.js',
-                        '../dlf/Resources/Public/Javascript/PageView/AnnotationControl.js',
-                        '../dlf/Resources/Public/Javascript/PageView/ImageManipulationControl.js',
-                        '../dlf/Resources/Public/Javascript/PageView/FulltextDownloadControl.js',
+                        '../dlf/Resources/Public/JavaScript/PageView/OLSources.js',
+                        '../dlf/Resources/Public/JavaScript/PageView/AltoParser.js',
+                        '../dlf/Resources/Public/JavaScript/PageView/AnnotationParser.js',
+                        '../dlf/Resources/Public/JavaScript/PageView/AnnotationControl.js',
+                        '../dlf/Resources/Public/JavaScript/PageView/ImageManipulationControl.js',
+                        '../dlf/Resources/Public/JavaScript/PageView/FulltextDownloadControl.js',
                         // custom scrolling - extend and override base FulltextControl
-                        '../dlf/Resources/Public/Javascript/PageView/FulltextControl.js',
+                        '../dlf/Resources/Public/JavaScript/PageView/FulltextControl.js',
                         'Resources/Private/JavaScript/Kitodo/PageView/FulltextControl.js',
-                        '../dlf/Resources/Public/Javascript/PageView/FullTextUtility.js',
+                        '../dlf/Resources/Public/JavaScript/PageView/FullTextUtility.js',
                         // use standard input instead of submit button
                         'Resources/Private/JavaScript/Kitodo/PageView/SearchInDocument.js',
                         // highlight more than one word separated by ' ' and keeping search
                         // phrase in search field
-                        '../dlf/Resources/Public/Javascript/PageView/PageView.js',
+                        '../dlf/Resources/Public/JavaScript/PageView/PageView.js',
                         // Customize OverviewMap control
                         'Resources/Private/JavaScript/Kitodo/PageView/PageView.js'
                     ],
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 TYPO3 extension to simplify work with and make adjustments to Kitodo.Presentation at [DDB-Zeitungsportal](https://www.deutsche-digitale-bibliothek.de/newspaper/).
 
-The JavaScript build is based on v4.0.1 (commit d8be5dd) of Kitodo.Presentation.
+The JavaScript build is based on v5.0.1 (commit 74f6343) of Kitodo.Presentation.
 
 ## Create unified JavaScript/CSS build
 
@@ -26,7 +26,7 @@ This builds:
 
 Example of dlf part of configuration from LocalConfiguration.php file is available [here](Documentation/LocalConfiguration.md). It can be used as a template for plugin configuration after the installation.
 
-## Upgrade to Kitodo.Presentation 4 (preliminary)
+## Upgrade to Kitodo.Presentation v4 / v5 (preliminary)
 
 - Upgrade package via Composer
 - In `LocalConfiguration.php`:
